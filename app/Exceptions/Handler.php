4ee26f955fdced1b69f2fc103414829d23ec5294
Riley Byrnes <byrnesriley@yahoo.com>
2017-03-21 18:45:46 -0500

    Used the command built into laravel to create authentication and registration functionality

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,8 +44,29 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
-    }
+        if($this->isHttpException($exception))
+        {
+            switch ($exception->getStatusCode())
+            {
+                // not found
+                case 404:
+                    return redirect()->guest('home');
+                    break;
+
+                // internal error
+                case '500':
+                    return redirect()->guest('home');
+                    break;
+
+                default:
+                    return $this->renderHttpException($exception);
+                    break;
+            }
+        }
+        else
+        {
+            return parent::render($request, $exception);
+        }    }
 
     /**
      * Convert an authentication exception into an unauthenticated response.
